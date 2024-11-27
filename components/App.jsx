e78cb2dec2994b105294cdf334e28f449473dd9f
Malek Mansour <malek24mansour@gmail.com>
2024-11-26 23:36:08 -0700

    Update App.jsx

--- components/App.jsx
+++ components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
-import { StyleSheet} from 'react-native';
+import { StyleSheet, View } from 'react-native'; 
 import { ToDoForm } from './ToDoForm';
 import { ToDoList } from './ToDoList';
 
@@ -8,11 +7,11 @@ function App() {
   const [tasks, setTasks] = useState([
     'Do laundry',
     'Go to gym',
-    'Walk dog'
+    'Walk dog',
   ]);
 
   const addTask = (task) => {
-    if (task.trim().length > 0 && !tasks.includes(task.trim())) { 
+    if (task.trim().length > 0 && !tasks.includes(task.trim())) {
       setTasks([...tasks, task.trim()]);
     }
   };
@@ -23,11 +22,12 @@ function App() {
       <ToDoForm addTask={addTask} />
     </View>
   );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#282c34', 
+    backgroundColor: '#282c34',
     padding: 20,
   },
 });
