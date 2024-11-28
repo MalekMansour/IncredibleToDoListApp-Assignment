4f899220509ed3770ed4bf9e356f2cad2b8848d0
Malek Mansour <malek24mansour@gmail.com>
2024-11-28 12:29:37 -0700

    Add App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import AboutScreen from './AboutScreen';
+
+// Define stack navigator
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Add the Home and About screens */}
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Home - To-Do List' }}
+        />
+        <Stack.Screen 
+          name="About" 
+          component={AboutScreen} 
+          options={{ title: 'About This App' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
