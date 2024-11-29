38f962d38aa3d4435de8cf06155ee99588d31326
Malek Mansour <malek24mansour@gmail.com>
2024-11-29 15:06:29 -0700

    Add AboutScreen.jsx

---
+++ src/screens/AboutScreen.jsx
@@ -0,0 +1,34 @@
+// src/screens/AboutScreen.jsx
+
+import React from 'react';
+import { Text, Button, StyleSheet } from 'react-native';
+import MainLayout from '../layouts/MainLayout';
+
+export default function AboutScreen({ navigation }) {
+  const currentDate = new Date().toLocaleDateString();
+
+  return (
+    <MainLayout>
+      <Text style={styles.title}>Incredible To-Do List App</Text>
+      <Text style={styles.subtitle}>Created by: Malek Mansour</Text>
+      <Text style={styles.subtitle}>Date: {currentDate}</Text>
+      <Button
+        title="Go Back to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </MainLayout>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    color: '#ffffff',
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#dcdcdc',
+  },
+});
