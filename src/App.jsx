a6284e9b4689de8b9033adde5c720385c14bd92f
Malek Mansour <malek24mansour@gmail.com>
2024-11-29 16:07:42 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
 
-const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        screenOptions={{
-          headerStyle: { backgroundColor: '#282c34' },
-          headerTintColor: '#fff',
-          drawerStyle: { backgroundColor: '#f0f0f0' },
-        }}
-      >
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="About" component={AboutScreen} />
-      </Drawer.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
