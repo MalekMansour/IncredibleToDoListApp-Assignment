834c80de3cc6bc047d9d71da42d9a1cff9747bc6
Malek Mansour <malek24mansour@gmail.com>
2024-11-29 16:48:57 -0700

    Update ToDoList.jsx

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,11 +1,11 @@
 import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
-export function ToDoList({ tasks }) {
+export function ToDoList({ tasks, deleteTask }) {
   return (
     <ScrollView>
       {tasks.map((task, index) => (
-        <Pressable key={index}>
+        <Pressable key={index} onPress={() => deleteTask(task)}>
           <View style={styles.task}>
             <Text style={styles.taskText}>{task}</Text>
           </View>
